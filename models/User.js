3618116d1d2e60bebf51af5f5b62aaf38a2b82d7
Syed Sumaim Ali <syedsumaimaly08@gmail.com>
2025-12-04 00:54:38 +0500

    Updating db.js and User.js file

--- models/User.js
+++ models/User.js
@@ -9,4 +9,13 @@ const UserSchema = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now }
 });
 
+
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  const bcrypt = require('bcrypt');
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
 module.exports = mongoose.model('User', UserSchema);
